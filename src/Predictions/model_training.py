6008bbb386af45cd90fee7fb675e121d73c74e5e
Beau Bellamy <beaubellamy@users.noreply.github.com>
2024-09-28 17:25:29 +1000

    added empty file for training and prediction

---
+++ src/Predictions/model_training.py
@@ -0,0 +1,12 @@
+
+
+
+if __name__ == "__main__":
+    # This is where the training of the model will be performed
+
+    # prediction should be predicting the wait time for (at least
+    # 30 mnin intervals) for the next day
+    pass
+
+
+
