db986214723bbccee9f3a94d02d4828f5a20fc81
Beau Bellamy <beaubellamy@users.noreply.github.com>
2025-03-16 13:23:00 +1100

    code cleanup and refactorisation

--- src/Predictions/model_predictions.py
+++ src/Predictions/model_predictions.py
@@ -2,9 +2,11 @@ import os.path
 import joblib
 import pickle
 import pandas as pd
+import numpy as np
 from datetime import datetime, timezone
 from src.settings import PROCESSED_FOLDER, MODELS_FOLDER, OUTPUT_FOLDER
 from src.Processing.processing_main import(
+    convert_datetime_to_min_since_midnight,
     convert_unix_date_to_datetime, convert_datetime_to_unix_date,
     feature_yesterday, feature_past7day_average,
     feature_sametime_lastweek, feature_sametime_lastmonth,
@@ -16,8 +18,10 @@ from src.Processing.processing_main import(
 if __name__ == "__main__":
 
     prediction_date = '2024-12-31'
-    model_name = 'Linear'
+    # model_name = 'Linear'
+    model_name = 'NeuralNetwork_2-32'
 
+    # Weather data for the day
     min_temp = 34
     avg_temp = 37
     max_temp = 39
@@ -34,7 +38,7 @@ if __name__ == "__main__":
     historical_data = os.path.join(PROCESSED_FOLDER, 'data_wth_features.csv')
     historical_df = pd.read_csv(historical_data)
     historical_df['Date_Time'] = pd.to_datetime(historical_df['Date_Time'])
-    historical_df['Date'] = convert_unix_date_to_datetime(historical_df['Date'])
+    # historical_df['Date'] = convert_unix_date_to_datetime(historical_df['Date'])
 
     # create a copy of 1 day to emulate the prediction day data
     prediction_df = historical_df[historical_df['Date'] == historical_df['Date'].max()].reset_index(drop=True)
@@ -42,7 +46,7 @@ if __name__ == "__main__":
      # update the date information
     # prediction_date = pd.to_datetime('28/01/2025')
     prediction_datetime = pd.to_datetime(prediction_date)
-    # prediction_df['Date'] = prediction_date
+    prediction_df['Date'] = prediction_datetime.date()
     prediction_df['Date_Time'] = pd.to_datetime(prediction_df['Date_Time'])
 
     prediction_df['Date_Time'] = prediction_df['Date_Time'].apply(
@@ -50,9 +54,10 @@ if __name__ == "__main__":
                             month=prediction_datetime.month,
                             day=prediction_datetime.day))
 
-    prediction_df['Date'] = pd.to_datetime(prediction_datetime)
-    prediction_df['Day'] = pd.to_datetime(prediction_datetime).weekday()
-    prediction_df['is_weekday'] = int(pd.to_datetime(prediction_datetime).weekday() < 5)
+    # prediction_df['Time'] = prediction_df['Date_Time'].dt.time
+    # # prediction_df['Date'] = pd.to_datetime(prediction_datetime)
+    # prediction_df['Day'] = prediction_datetime.weekday()
+    # prediction_df['is_weekday'] = int(prediction_datetime.weekday() < 5)
 
 
     # rolling measures
@@ -63,9 +68,18 @@ if __name__ == "__main__":
 
     # append the prediction day to the hisorical data to include the hstorical rolling averages
     prediction_df = pd.concat([historical_df, prediction_df]).reset_index(drop=True)
-    feature_df = prediction_df[['Date_Time', 'Date', 'Time', 'Ride', 'Day', 'is_weekday', 'Wait Time']]
+    prediction_df['Time'] = prediction_df['Date_Time'].dt.time
+    prediction_df['Day'] = prediction_df['Date_Time'].dt.weekday
+    prediction_df['is_weekday'] = (prediction_df['Date_Time'].dt.weekday < 5).astype(int)
 
-    # todo: 'Date' field is not a common tye.
+    feature_df = prediction_df[['Date_Time', 'Date', 'Time', 'Ride', 'Day', 'is_weekday', 'Wait Time']]
+    # feature_df = prediction_df[['Date', 'Time', 'Ride', 'Day', 'is_weekday', 'Wait Time']]
+    # todo: think about how i need to address the date time fields - processed as cyclic fields and no longer have teh raw info
+    # convert back to time, or process the cycle before calcaulting the new features in processing step
+    feature_df['Date'] = pd.to_datetime(feature_df['Date'])
+    feature_df['Date_Time'] = pd.to_datetime(feature_df['Date_Time'])
+
+    # todo: 'Date' field is not a common type.
     yesterday_df = feature_yesterday(feature_df)
     past7day = feature_past7day_average(feature_df)
     df7days_ago = feature_sametime_lastweek(feature_df)
@@ -81,7 +95,8 @@ if __name__ == "__main__":
     feature_df = pd.merge(feature_df, hourly_trend_df, how='left', on=['Ride', 'Date_Time'])
     feature_df = pd.merge(feature_df, three_hourly_trend_df, how='left', on=['Ride', 'Date_Time'])
 
-    feature_df['Date'] = convert_datetime_to_unix_date(feature_df['Date'])
+    # feature_df['Date'] = convert_datetime_to_unix_date(feature_df['Date'])
+    # feature_df['Time'] = convert_datetime_to_min_since_midnight(feature_df['Date_Time'])
 
     # prediction_df['Date'] = int(pd.to_datetime(prediction_date).timestamp())
 
@@ -93,33 +108,56 @@ if __name__ == "__main__":
     # weather from public forecasts
     feature_df['Max Temp'] = max_temp
     feature_df['Avg Temp'] = avg_temp
-    feature_df['min Temp'] = min_temp
-    feature_df['Precipitation'] = precipitation
+    # feature_df['min Temp'] = min_temp
+    # feature_df['Precipitation'] = precipitation
     # todo: merge in current weather data
+    feature_df['Time'] = convert_datetime_to_min_since_midnight(feature_df['Time'])
+    feature_df['Day_sin'] = np.sin(2 * np.pi * feature_df['Day'] / 7)
+    feature_df['Day_cos'] = np.cos(2 * np.pi * feature_df['Day'] / 7)
+    feature_df['Time_sin'] = np.sin(2 * np.pi * feature_df['Time'] / 1440)
+    feature_df['Time_cos'] = np.cos(2 * np.pi * feature_df['Time'] / 1440)
+    feature_df.drop(columns=['Date', 'Day', 'Time'], inplace=True)
 
-    feature_df = pd.get_dummies(feature_df, columns=['Ride'])
+    # feature_df = pd.get_dummies(feature_df, columns=['Ride'])
+    # Loop through each ride and load the required model
+    prediction = pd.DataFrame()
+    for ride in feature_df['Ride'].unique().tolist():
+        ride_label = ride.replace(':', '').replace(' ', '_')
 
-    # load the prefered model
-    model = joblib.load(os.path.join(MODELS_FOLDER, f'{model_name}.pkl'))
+        # load the prefered model
+        model = joblib.load(os.path.join(MODELS_FOLDER, f'{model_name}_{ride_label}.pkl'))
+        scaler = joblib.load(os.path.join(MODELS_FOLDER, f'{ride_label}_scalar.pkl'))
 
-    scaler = joblib.load(os.path.join(MODELS_FOLDER, 'scalar.pkl'))
-    feature_df = feature_df[['Date', 'Time', 'Day', 'is_weekday', 'Max Temp', 'Avg Temp', 'min Temp',
-     'Precipitation', 'Yesterday_wait_time', 'Rolling_Avg_7_Days',
-     'LastWeek_wait_time', 'LastMonth_wait_time',
-     'Ride_Avengers Assemble: Flight Force',
-     'Ride_Buzz Lightyear Laser Blast', 'Ride_Cars Quatre Roues Rallye',
-     'Ride_Cars ROAD TRIP', "Ride_Crush's Coaster", 'Ride_Indiana Jones and the Temple of Peril',
-     'Ride_Les Mysteres du Nautilus', 'Ride_Orbitron', 'Ride_Phantom Manor',
-     'Ride_Pirates of the Caribbean', 'Ride_Ratatouille: The Adventure',
-     'Ride_Slinky Dog Zigzag Spin', 'Ride_Spider-Man W.E.B. Adventure',
-     'Ride_Star Tours - The Adventure Continues',
-     'Ride_Star Wars Hyperspace Mountain',
-     'Ride_The Twilight Zone Tower of Terror',
-     'Ride_Toy Soldiers Parachute Drop']]
-    features = scaler.transform(feature_df)
+        prediction_df = feature_df[feature_df['Ride'] == ride]
+        prediction_df = prediction_df[['is_weekday', 'Max Temp', 'Avg Temp',
+                                  'Yesterday_wait_time', 'Rolling_Avg_7_Days',
+                                  'LastWeek_wait_time', 'LastMonth_wait_time',
+                                  'Rolling_28D_hr_trend', 'Rolling_28D_3hr_trend',
+                                  'Day_sin', 'Day_cos', 'Time_sin', 'Time_cos']]
 
-    # make the predictions
-    feature_df['pred_wait_time'] = model.predict(features)
+
+        # 'Ride_Avengers Assemble: Flight Force',
+        # 'Ride_Buzz Lightyear Laser Blast', 'Ride_Cars Quatre Roues Rallye',
+        # 'Ride_Cars ROAD TRIP', "Ride_Crush's Coaster", 'Ride_Indiana Jones and the Temple of Peril',
+        # 'Ride_Les Mysteres du Nautilus', 'Ride_Orbitron', 'Ride_Phantom Manor',
+        # 'Ride_Pirates of the Caribbean', 'Ride_Ratatouille: The Adventure',
+        # 'Ride_Slinky Dog Zigzag Spin', 'Ride_Spider-Man W.E.B. Adventure',
+        # 'Ride_Star Tours - The Adventure Continues',
+        # 'Ride_Star Wars Hyperspace Mountain',
+        # 'Ride_The Twilight Zone Tower of Terror',
+        # 'Ride_Toy Soldiers Parachute Drop']]
+        features = scaler.transform(prediction_df)
+
+        # make the predictions
+        prediction_df['pred_wait_time'] = model.predict(features)
+        prediction_df['Ride'] = ride
+
+        prediction = pd.concat([prediction, prediction_df])
+        # todo: add predictions to a single file - single DF
+
+
+    prediction['Time'] = (np.arctan2(prediction['Time_sin'], prediction['Time_cos']) / (2 * np.pi)) * 1440
+    prediction['Time'] %= 1440
 
     output_filename = f'predictions_{prediction_date.replace('-','')}.csv'
-    feature_df.to_csv(os.path.join(OUTPUT_FOLDER, output_filename))
+    prediction.to_csv(os.path.join(OUTPUT_FOLDER, output_filename))
